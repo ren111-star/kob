d949d94e92c90cec35a64d9b1b9deb48613c0f88
fuxiang ren <1521960198@qq.com>
2022-08-30 21:01:54 +0800

    5.1 创建个人中心页面（上）

--- web/src/views/user/bots/UserBotIndexView.vue
+++ web/src/views/user/bots/UserBotIndexView.vue
@@ -4,10 +4,83 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex'
 export default {
     components: {
     ContentField
-}
+    },
+    setup() {
+        const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/add/",
+        //     type: "post",
+        //     data: {
+        //         title: "Bot",
+        //         description: "description",
+        //         content: "demo",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/remove/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 2,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+            // $.ajax({
+            //     url: "http://127.0.0.1:3000/user/bot/update/",
+            //     type:"post",
+            //     data: {
+            //         bot_id: 3,
+            //         title: "bot的标题",
+            //         description: "bot的描述",
+            //         content: "bot的代码",
+            //     }, 
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     }
+            // })
+            $.ajax({
+                url: "http://127.0.0.1:3000/user/bot/getlist/",
+                type: "get",
+                headers: {
+                    Authorization: "Bearer " + store.state.user.token,
+                },
+                success(resp) {
+                    console.log(resp);
+                },
+                error(resp) {
+                    console.log(resp);
+                }
+            })
+
+    }
 }
 </script>
 
